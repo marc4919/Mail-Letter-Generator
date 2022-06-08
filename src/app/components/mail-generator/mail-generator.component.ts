a0553ee6b30e45c75221c7bfbc823530b0357600
Marc4919 <marcgeek4919@gmail.com>
2022-06-08 17:39:26 +0200

    Solucionado error import

--- src/app/components/mail-generator/mail-generator.component.ts
+++ src/app/components/mail-generator/mail-generator.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfMake/build/vfs_fonts';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
